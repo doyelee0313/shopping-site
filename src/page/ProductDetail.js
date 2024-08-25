8e95ed4eeb2bbcef4b73b6aebc989e85c4637956
doyelee0313 <doyelee0313@gmail.com>
2024-08-25 23:27:36 +0800

    fixed

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,43 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col } from 'react-bootstrap';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 
 const ProductDetail = () => {
-  let{id} = useParams();
+  let { id } = useParams();
   let [product, setProduct] = useState(null);
-  const getProductDetail = async() => {
-    let url = `https://my-json-server.typicode.com/doyelee0313/shopping-site/products/${id}`
+
+  const getProductDetail = async () => {
+    let url = `https://my-json-server.typicode.com/doyelee0313/shopping-site/products/${id}`;
     let response = await fetch(url);
     const data = await response.json();
-    console.log(data);
     setProduct(data);
   };
+
   useEffect(() => {
     getProductDetail();
-  }, [])
+  }, [id]);
+
   return (
     <Container>
-      <Row>
-        <Col classname="product-img">
-          <img src={product?.img}></img>
+      <Row className="product-detail">
+        <Col xs={12} md={6} className="d-flex justify-content-center align-items-center">
+          <img src={product?.img} alt={product?.title} className="product-img" />
         </Col>
-        <Col>
-        <div className="content">
-          <div>{product?.title}</div>
-            <div>{"$"}{product?.price}</div>
-            <DropdownButton id="dropdown-basic-button" title="size">
-            <Dropdown.Item href="#/action-1">S</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">M</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">L</Dropdown.Item>
+        <Col xs={12} md={6} className="d-flex justify-content-center align-items-center">
+          <div className="content">
+            <h1 className="product-title">{product?.title}</h1>
+            <p className="product-price">{"$"}{product?.price}</p>
+            <DropdownButton id="dropdown-basic-button" title="Size" className="dropdown-button">
+              <Dropdown.Item href="#/action-1">S</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">M</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">L</Dropdown.Item>
             </DropdownButton>
-            <button>BUY</button>
-        </div>
+            <button className="buy-button">BUY</button>
+          </div>
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
